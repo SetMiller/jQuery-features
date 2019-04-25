6d631f34a40ee0e1d60202448cf175a9a26d2be3
SetMiller <setmiller@gmail.com>
2019-04-26 01:56:43 +0300

    upd

--- features/02_jQuery/script.js
+++ features/02_jQuery/script.js
@@ -1,18 +1,35 @@
 window.onload = function (){
-  const quest = $('.wrapper').children('.question');
-  const answer = $('.wrapper').children('.answer');
+  const allQuest = $('.wrapper').children('.question');
+  const allAnswers = $('.wrapper').children('.answer');
   
-  quest.each(function(index){
+  allQuest.each(function(index){
           $(this).html(`Вопрос №${index + 1}`);
         })
-        .on('click', function(){
-          answer.slideUp(300);
-            $(this).next().css('display') === 'block' ? 
-            $(this).next().slideUp(300) : 
-            $(this).next().slideDown(300)
-        })
-
-  answer.each(function(index){
+  allAnswers.each(function(index){
           $(this).html(`Ответ на вопрос №${index + 1}`);
         });
+
+  // Вариант № 3 вне задания (аккордеон)
+  $('.wrapper .answer:first').show();
+
+  allQuest.on('click', function(){
+          const thisAnswer = $(this).next()
+        // Вариант решения № 1
+          // allAnswers.slideUp(300);
+          //   // Если между лежит несколько промежуточных блоков или элементов
+          //   // то можно сделать $(this).parent('class').find('class') (запись примерная)
+          //   thisAnswer.css('display') === 'block' ? 
+          //   thisAnswer.slideUp(300) : 
+          //   thisAnswer.slideDown(300)
+
+      //  Вариант решения № 2
+            // флаг visible помогает отфильтровать только видимые элементы
+            // $('.wrapper .answer:visible').not(thisAnswer).slideUp(300)
+            // thisAnswer.slideToggle(300)
+
+      // Вариант № 3 вне задания (аккордеон)
+            $('.wrapper .answer:visible').not(thisAnswer).slideUp(300);
+            $(this).next().slideDown(300);
+        })
+
 }
